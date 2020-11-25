e0ec23e5d2d1dde88dfb9c8ca7115e7d08460284
moraagarcia <moragarcia2007@hotmail.com>
2020-11-25 01:21:53 -0300

    paso mcd a recursiva

--- mcfunctions.c
+++ mcfunctions.c
@@ -1,11 +1,6 @@
-unsigned int mcd(unsigned int n1, unsigned int n2) {
-    unsigned int n_aux;
-    while (n2 != 0) {
-        n_aux = n2;
-        n2 = n1 % n2;  //n2 = residuo de n1/n2
-        n1 = n_aux;  //n1 toma el valor de n2 previo a ser asignado con el residuo
-    }
-    return n1;
+unsigned int mcd(unsigned int n1, unsigned int n2){
+    if (n2 == 0) return n1;
+    return mcd(n2, n1%n2);
 }
 
 unsigned int mcm(unsigned int n1, unsigned int n2) {
